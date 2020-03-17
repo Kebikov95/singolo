0723ad0f30c71032654130809dbbf98a26dd1c16
Kebikau Kiryl <kebikov1995@gmail.com>
2020-03-17 18:32:32 +0300

    feat: add functionality for portfolio block

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const BUTTON_LEFT= document.getElementById('btn-left');
 const BUTTON_RIGHT= document.getElementById('btn-right');
 const PHONE_V = document.getElementById('phone-vertical');
 const PHONE_H = document.getElementById('phone-horizontal');
+const PORTFOLIO_MENU = document.getElementById('portfolio-btn');
+const PHOTOS = document.getElementById("photo");
 
 
 /* Header */
@@ -57,3 +59,80 @@ function offAllScreens(){
 
 PHONE_V.addEventListener('click', offVerticalScreen);
 PHONE_H.addEventListener('click', offHorizontalScreen);
+
+
+/* Portfolio */
+PORTFOLIO_MENU.addEventListener('click', (event) => {
+    PORTFOLIO_MENU.querySelectorAll('.btn').forEach(el => { el.classList.remove('active') });
+    event.target.classList.add('active');
+    let photosArray = document.querySelectorAll('div.photos > img');
+    let point = event.target.innerText;
+    switch(point){
+        case 'All':
+            photosArray[0].style.order="0";
+            photosArray[1].style.order="10";
+            photosArray[2].style.order="2";
+            photosArray[3].style.order="3";
+            photosArray[4].style.order="4";
+            photosArray[5].style.order="5";
+            photosArray[6].style.order="6";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="8";
+            photosArray[9].style.order="9";
+            photosArray[10].style.order="1";
+            photosArray[11].style.order="11";
+            break;
+        case 'Web Design':
+            photosArray[0].style.order="8";
+            photosArray[1].style.order="2";
+            photosArray[2].style.order="1";
+            photosArray[3].style.order="9";
+            photosArray[4].style.order="4";
+            photosArray[5].style.order="3";
+            photosArray[6].style.order="11";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="0";
+            photosArray[9].style.order="5";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="6";
+            break;
+        case 'Graphic Design':
+            photosArray[0].style.order="4";
+            photosArray[1].style.order="7";
+            photosArray[2].style.order="1";
+            photosArray[3].style.order="5";
+            photosArray[4].style.order="0";
+            photosArray[5].style.order="2";
+            photosArray[6].style.order="6";
+            photosArray[7].style.order="3";
+            photosArray[8].style.order="8";
+            photosArray[9].style.order="9";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="11";
+            break;
+        case 'Artwork':
+            photosArray[0].style.order="4";
+            photosArray[1].style.order="2";
+            photosArray[2].style.order="6";
+            photosArray[3].style.order="9";
+            photosArray[4].style.order="8";
+            photosArray[5].style.order="3";
+            photosArray[6].style.order="11";
+            photosArray[7].style.order="7";
+            photosArray[8].style.order="0";
+            photosArray[9].style.order="5";
+            photosArray[10].style.order="10";
+            photosArray[11].style.order="1";
+            break;
+    }
+});
+
+PHOTOS.addEventListener('click', (event) => {
+    if(event.target.classList.contains('active')) {
+        event.target.classList.remove('active');
+    }
+    else {
+        PHOTOS.querySelectorAll('img').forEach( el => el.classList.remove('active') );
+        event.target.classList.add('active');
+    }
+})
